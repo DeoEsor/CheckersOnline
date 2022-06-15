29c65239110273b384b52dcce9f29634d11e28f2
DeoEsor <RulianValeriy@yandex.ru>
2022-06-15 07:08:30 +0300

    [Desktop] WPF Controls

--- CheckersControls/Desk.xaml.cs
+++ CheckersControls/Desk.xaml.cs
@@ -13,22 +13,51 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Models.Checkers;
+using Color = System.Drawing.Color;
 
 namespace CheckersControls
 {
     /// <summary>
     /// Логика взаимодействия для Desk.xaml
     /// </summary>
-    public partial class Desk : UserControl
+    public sealed partial class Desk : UserControl
     {
         public Board Board { get; set; }
+
+        public IEnumerable<char> Numbers => Enumerable
+            .Range(BoardGrid.Columns, 1)
+            .ToList()
+            .Select(s => (char)(s + 30));
         
-        public IEnumerable<char> Numbers => "87654321";
-        public IEnumerable<char> Letters => "ABCDEFGH";
+        public IEnumerable<char> Letters => 
+            Enumerable
+                .Range(1, BoardGrid.Rows)
+                .ToList()
+                .Select(s => (char) (s + 40));
         
-        public Desk()
+        public Desk(Board board)
         {
+            Board = board;
             InitializeComponent();
+            BoardGrid.Rows = Board.GetDimensions().Item1;
+            BoardGrid.Columns = Board.GetDimensions().Item2;
+
+            for (var i = 0; i < BoardGrid.Columns; i++)
+                for (var j = 0; j < BoardGrid.Rows; j++)
+                    AddCell(j, i);
+            
+            BoardGrid.UpdateLayout();
+        }
+
+        private void AddCell(int x, int y)
+        {
+            var cell = new Cell(Board,x,y,(x + y) % 2 == 0 ? Color.Black : Color.White);
+
+            Grid.SetColumn(cell, y);
+            Grid.SetRow(cell, x);
+
+            if (!BoardGrid.Children.Contains(cell))
+                BoardGrid.Children.Add(cell);
         }
     }
 }
